ebd021b59e8cfcd75947bde317807b6bbbcd0ef6
Tom Vogel <tom.vogel@appcarousel.com>
2017-07-21 12:00:11 -0700

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ let server = http.createServer(function (request, response) {
 
   response.writeHead(200, {"Content-Type": "text/plain"});
 
-  response.end("Hello Delivery Pipeline\n");
+  response.end("Hello Developers\n");
 
 });
 
